dbef87893d1c09f23190fdca284600fce49e1cb7
Pranjal Upadhyaya <106292533+pranjal-upadhyaya@users.noreply.github.com>
2024-04-17 18:40:51 +0530

    update task.py

--- task/task.py
+++ task/task.py
@@ -1,3 +1,6 @@
+import sqlite3
+import time
+
 class Task:
 
     def __init__(self, job_id, key, input: dict):
@@ -7,34 +10,66 @@ class Task:
         self.idempotency_key = key
         self.country = country
         self.state_list = state_list
+        self.run_task()
 
     def run_sub_task(self, state: str, retries = 3):
         successfull_run = False
         if retries > 0:
             # The code to run subtask exists here. If the code block runs succesfully, set succesfull_run = True
+            try:
+                print("state")
+                successfull_run = True
+            except Exception as e:
+                pass
             if not successfull_run:
                 successfull_run = self.run_sub_task(state, retries = retries - 1)
+        # print(successfull_run)
         return successfull_run
     
     def run_task(self):
         for state in self.state_list:
+            print(state)
+            time.sleep(5)
+            con = sqlite3.connect("task.db")
+            cur = con.cursor()
             query = "select * from task where country = ? and state = ?"
-            params = (self.country, state)
-            task_table_entry = sql.execute(query=query, params=params).fetchone()
+            params = (self.country, state, )
+            task_table_entry = cur.execute(query, params, ).fetchone()
             if task_table_entry is None:
-                query = f"insert into task(idempotency_key, country, state, job_id, status_code) select {self.idempotency_key}, {self.id}, {self.country}, {state}, 'RUNNING'"
-                sql.execute(query=query)
+                query = "insert into task(idempotency_key, country, state, job_id, status_code) values (?, ?, ?, ?, ?)"
+                params = (self.idempotency_key, self.country, state, self.job_id, "RUNNING", )
+                cur.execute(query, params)
+                con.commit()
                 sub_task_result_status = self.run_sub_task(state, retries=3)
+                print(sub_task_result_status)
                 if sub_task_result_status:
-                    continue
+                    query = "update task set status_code = 'SUCCESS' where country = ? and state = ?"
+                    params = (self.country, state)
+                    cur.execute(query, params, )
+                    con.commit()
                 else:
+                    query = "update task set status_code = 'FAILURE' where country = ? and state = ?"
+                    params = (self.country, state)
+                    cur.execute(query, params, )
+                    con.commit()
                     break
             else:
-                status_code = task_table_entry.status_code
+                status_code = task_table_entry[3]
                 if status_code == "SUCCESS":
-                    pass
+                    continue
                 elif status_code == "FAILURE":
-                    self.run_sub_task(state=state)
+                    sub_task_result_status = self.run_sub_task(state, retries=3)
+                    if sub_task_result_status:
+                        query = "update task set status_code = 'SUCCESS' where country = ? and state = ?"
+                        params = (self.country, state)
+                        cur.execute(query, params, )
+                        con.commit()
+                    else:
+                        query = "update task set status_code = 'FAILURE' where country = ? and state = ?"
+                        params = (self.country, state)
+                        cur.execute(query, params, )
+                        con.commit()
+                        break
                 else:
                     break
         
